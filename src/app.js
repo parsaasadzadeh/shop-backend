65daa541ea89af883d1c882103df2645c976753f
parsaasadzadeh <fightingparsa@gmail.com>
2026-06-25 14:39:14 +0330

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
+// app.js
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const path = require("path");
-
 const routes = require("./routes");
 const notFoundMiddleware = require("./middlewares/notFound.middleware");
 const errorMiddleware = require("./middlewares/error.middleware");
+const connectDB = require("./config/db"); // اضافه کردن این خط
 
 const app = express();
 
-// app.use(helmet());
+// اجرای تابع اتصال به دیتابیس در هر ریکوئست (بهینه‌شده برای Serverless)
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "Database connection failed" });
+  }
+});
+
 app.use(
   helmet({
     crossOriginResourcePolicy: {
@@ -18,13 +28,13 @@ app.use(
     },
   })
 );
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN || "*",
     credentials: true,
   })
 );
-
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -42,7 +52,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api", routes);
-
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 

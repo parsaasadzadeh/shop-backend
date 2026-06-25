7eefb9560a59bd78bc3d14c50f3b73cfceb8e52c
parsaasadzadeh <fightingparsa@gmail.com>
2026-06-25 14:38:49 +0330

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,28 @@
+// config/db.js
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  const mongoUri = process.env.MONGODB_URI;
+// متغیری برای ذخیره وضعیت اتصال تا در ریکوئست‌های بعدی دوباره متصل نشویم
+let isConnected = false; 
 
+const connectDB = async () => {
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  const mongoUri = process.env.MONGODB_URI;
   if (!mongoUri) {
     throw new Error("MONGO_URI is not defined in environment variables");
   }
 
-  const conn = await mongoose.connect(mongoUri);
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
+  try {
+    const db = await mongoose.connect(mongoUri);
+    isConnected = db.connections[0].readyState === 1;
+    console.log(`MongoDB Connected: ${db.connection.host}`);
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error.message);
+    throw error;
+  }
 };
 
 module.exports = connectDB;
